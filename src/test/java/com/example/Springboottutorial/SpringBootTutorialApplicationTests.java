b058b8ac63ef2f08af16b3cf6ad70d49a971ccb5
suryakatari97 <surya.katari@sjsu.edu>
2022-09-30 16:36:48 -0700

    initial commit

---
+++ src/test/java/com/example/Springboottutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
