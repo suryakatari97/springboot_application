439120f8309c927477a9ee762c41308a43ed4077
suryakatari97 <surya.katari@sjsu.edu>
2022-10-04 17:55:41 -0700

    CRUD APIs

---
+++ src/main/java/com/example/Springboottutorial/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.example.Springboottutorial.service;
+
+import com.example.Springboottutorial.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
